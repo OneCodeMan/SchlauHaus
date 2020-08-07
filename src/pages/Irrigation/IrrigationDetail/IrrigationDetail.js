f80f34685ec267606361c9f5dff00f9e6d9df91f
dave gumba <dave_gumba@yahoo.com>
2020-08-07 17:44:41 -0400

    gonna refactor frequency component to its own

--- src/pages/Irrigation/IrrigationDetail/IrrigationDetail.js
+++ src/pages/Irrigation/IrrigationDetail/IrrigationDetail.js
@@ -1,25 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import Switch from 'react-switch';
 import Button from 'react-bootstrap/Button';
 import '../../details.css';
 import './IrrigationDetail.css';
 
-function IrrigationDetail() {
+const fadeStyle = {
+  opacity: '0.5'
+};
 
-  return (
-    <div className="irrigationDetail">
+class IrrigationDetail extends Component {
+  constructor() {
+    super();
+    this.state = { checked: true };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-      <div className="backButton">
-        <Link to="/irrigation">
-          <Button variant="dark">
-            Back to Irrigation Page
+  handleChange(checked) {
+    this.setState({ checked });
+  }
+
+  render() {
+    return (
+      <div className="irrigationDetail">
+
+        <div className="titleDiv textCenter">
+          <h1 className="title">Sprinkler Detail</h1>
+          <h3 className="subTitle">83 Öbst Straße, All Sprinklers</h3>
+        </div>
+  
+        <div className="toggle textCenter">
+          <h5 className="scheduled">SCHEDULED</h5>
+          <label className="toggleSwitchContainer">
+            <Switch 
+              className="scheduleSwitch"
+              onChange={this.handleChange}
+              checked={this.state.checked}
+              handleDiameter={30}
+              onColor="#613DC1"
+              uncheckedIcon={false}
+              checkedIcon={false}
+              height={35}
+              width={70}
+              boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+              activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+            />
+          </label>
+        </div>
+
+        <div className="frequencyDiv">
+          <h4 className="tertiaryTitle textCenter">Frequency</h4>
+          <div className="frequencySelection">
+            <div className="daySetting">
+
+
+              <div className="dayWrapper">
+                <div className="check">
+                  ✅
+                </div>
+                <div className="day">
+                  <p>M</p>
+                </div>
+              </div>
+
+              <div className="dayWrapper">
+                <div className="check">
+                  ✅
+                </div>
+                <div className="day">
+                  <p>T</p>
+                </div>
+              </div>
+              
+
+            </div>
+          </div>
+        </div>
+
+        <div className="durationDiv">
+          <h4 className="tertiaryTitle textCenter">Duration</h4>
+          <div className="durationSettings textCenter">
+            <div className="timeSetting" style={fadeStyle}>
+              <input type="time" />
+              <p className="label">Time</p>
+            </div>
+            <div className="minutesSetting">
+              <input type="number" min="2" max="20" className="numberInput"/>
+              <p className="label">Duration (mins)</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="confirmButtonDiv textCenter">
+          <Button variant="primary">
+            Confirm
           </Button>
-        </Link>
+        </div>
+  
+        <div className="backButton">
+          <Link to="/irrigation">
+            <Button variant="dark">
+              Back to Irrigation Page
+            </Button>
+          </Link>
+        </div>
+  
       </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default IrrigationDetail;
